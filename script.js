784c8d7687e8f56deb99fa56afea6ba69ef9398c
negser <94227501+negser@users.noreply.github.com>
2026-01-20 13:29:23 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Función para abrir y cerrar el panel lateral
-function toggleSidebar() {
-  var sidebar = document.getElementById("sidebar");
-  sidebar.classList.toggle("active"); // Alterna la clase 'active' para mostrar u ocultar el sidebar
-}
